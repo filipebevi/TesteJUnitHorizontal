8bba7e3538956e1cbf41aa668c2dfdee7e1f9305
Bevilaqua <filipe.s.bevilaqua@accenture.com>
2020-07-12 18:14:29 -0300

    Adicionado a variavel serializable

--- src/main/java/com/accenture/acme/store/shippingservice/ZipCodeNotFoundException.java
+++ src/main/java/com/accenture/acme/store/shippingservice/ZipCodeNotFoundException.java
@@ -1,6 +1,12 @@
 package com.accenture.acme.store.shippingservice;
 
 public class ZipCodeNotFoundException extends Exception {
+    
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
     public ZipCodeNotFoundException(String message) {
         super(message);
     }
